95c366b153b05b3e62d6963be74510007ac18df5
patunia <pborowiec@alumni.nd.edu>
2018-07-31 22:32:12 -0500

    homepage creatiom

---
+++ my-app/src/Pages/Home.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import NavBar from "../components/NavBar";
+import Container from "../components/Container";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import data from "../../turtle.json";
