a547cb46860733ddda01f07f6f4732341ea4fcf2
patunia <pborowiec@alumni.nd.edu>
2018-07-31 22:38:08 -0500

    not broken yet, homepage turtles

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -4,3 +4,47 @@ import Container from "../components/Container";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import data from "../../turtle.json";
+
+class Home extends Component {
+    state = {
+        turtles: [],
+        score: 0,
+        topScore: 0,
+        selectedturtle: [], 
+        scores: '',
+    };
+
+    componentDidMount() {
+        this.loadDefaultScore();
+    };
+
+    loadDefaultScore = () => {
+        this.setState({
+            turtles: turtle,
+            score: 0,
+            topScore: 0,
+            selectedturtle: [],
+            scores: 'Select one image to start game!',
+        });
+    };
+
+    render() {
+        return (
+          <div>
+            <Nav score={this.state.score} topScore={this.state.topScore} />
+            <Header />
+            <Container>
+              {this.state.data.map(item => (
+                <ClickItem
+                  key={item.id}
+                  id={item.id}
+                  shake={!this.state.score && this.state.topScore}
+                  handleClick={this.handleItemClick}
+                  image={item.image}
+                />
+              ))}
+            </Container>
+            <Footer />
+          </div>
+
+export default Home;
